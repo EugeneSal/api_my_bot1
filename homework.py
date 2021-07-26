c5c88b387324c4286bfc9a3ce144565e3d183e5c
EugeneSal <umbra-mortis@mail.ru>
2021-07-26 20:21:24 +0800

    bot 8 sprint ver 2.5 refactoring of function weather30

--- homework.py
+++ homework.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 from logging.handlers import RotatingFileHandler
 import os
 import time
@@ -20,14 +21,15 @@ except Exception as e:
     logging.error(token_error)
 if PRAKTIKUM_TOKEN is None or TELEGRAM_TOKEN is None or CHAT_ID is None:
     token_error = 'Не получены ТОКЕН.'
-    logging.error(token_error)
+    logging.critical(token_error)
     bot.send_message(CHAT_ID, token_error)
+    sys.exit()
 URL = 'https://praktikum.yandex.ru/api/{}'
 updater = Updater(TELEGRAM_TOKEN, use_context=True)
 
 
 logging.basicConfig(
-    level=logging.ERROR,
+    level=logging.DEBUG,
     format='%(asctime)s, %(message)s, %(levelname)s, %(name)s')
 handler = RotatingFileHandler('main.log', maxBytes=50000000, backupCount=5)
 logging.getLogger("").addHandler(handler)
@@ -49,7 +51,7 @@ def parse_homework_status(homework):
             f'Комментарий: {homework.get("reviewer_comment")}')
 
 
-def get_homeworks(current_timestamp=int(time.time())):
+def get_homeworks(current_timestamp):
     url = URL.format('user_api/homework_statuses/')
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     try:
